fa44fd9904a25bf26f46a74eb1a28bfa569bad53
Raja <prabakaranrvp@gmail.com>
2017-12-04 17:17:08 +0530

    Heroku autotest

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-  res.send('Welocome to the test App');
+  res.send('Welocome to the test App!');
 })
 
 app.listen(port, function() {
