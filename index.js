b42e1d78c2805c2d1e5fad6ea07fe66db4b11166
Raja <prabakaranrvp@gmail.com>
2017-12-04 11:54:38 +0530

    Heroku Modifications

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const fs = require('fs');
 
 const app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
   res.send('Welocome to the test App');
 })
 
-app.listen(80, function() {
-  console.log("Started Server on http://localhost:80/")
+app.listen(port, function() {
+  console.log("Started Server on http://localhost:" + port "/")
 });
