b1b3a3a4973661d2b52e34c7663da7eafd557701
Raja <prabakaranrvp@gmail.com>
2017-12-04 12:00:55 +0530

    v2

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 
 const app = express();
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
   res.send('Welocome to the test App');
 })
 
 app.listen(port, function() {
-  console.log("Started Server on http://localhost:" + port "/")
+  console.log("Started Server on http://localhost:" + port + "/");
 });
